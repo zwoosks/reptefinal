6b0ce9dfafdd9b39c2cca921b2b79cf93a63864e
zwoosks <32276266+zwoosks@users.noreply.github.com>
2019-06-12 18:38:33 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,19 @@
 def startBoard():
     return [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     
+def drawBoard(l, n):
+    if len(l) < 9:
+        print "Error! Board badly designed!"
+    else:
+        print "Turn number: " + str(n)
+        print "============"
+        taula(l[6:9])
+        print "------------"
+        taula(l[3:6])
+        print "------------"
+        taula(l[0:3])
+        print "============"
+def taula(l):
+    print "   |   |   "
+    print " " + l[0] + " | " + l[1] + " | " + l[2] + " "
+    print "   |   |   "
